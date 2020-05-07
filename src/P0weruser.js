995f57c212a017234aa1dd5e6357e760179df457
Scarwolf <pottii@me.com>
2020-05-07 21:56:33 +0200

    Remove c0mmunityrat Module

--- src/P0weruser.js
+++ src/P0weruser.js
@@ -1,6 +1,5 @@
 import Chat from './module/Chat';
 import Pr0p0ll from './module/Pr0p0ll';
-import C0mmunityrat from './module/C0mmunityrat';
 import ViewedPostsMarker from './module/ViewedPostsMarker';
 import Settings from './Settings';
 import Utils from './Utils';
@@ -60,7 +59,6 @@ export default class P0weruser {
         return JSON.parse(modules);
     }
 
-
     static saveActivatedModules(selection) {
         window.localStorage.setItem('activated_modules', JSON.stringify(selection));
     }
@@ -90,7 +88,6 @@ export default class P0weruser {
                 'ImageOCR': new ImageOCR(),
                 'Pr0p0ll': new Pr0p0ll(),
                 'ViewedPostsMarker': new ViewedPostsMarker(),
-                'C0mmunityrat': new C0mmunityrat(),
                 'Chat': new Chat(),
                 'AnonymousTitle': new AnonymousTitle(),
                 'StatisticsLinkInNavbar': new StatisticsLinkInNavbar(),
