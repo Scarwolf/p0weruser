5119b1c406a6ac9a4bad7f8bbb13d59350c9f8b9
tobi6112 <22715034+tobi6112@users.noreply.github.com>
2021-07-31 19:15:34 +0200

    Add option to opt-out from marking favorites as seen. Closes #41

--- src/module/ViewedPostsMarker.js
+++ src/module/ViewedPostsMarker.js
@@ -6,6 +6,7 @@ export default class ViewedPostsMarker {
         this.id = 'ViewedPostsMarker';
         this.name = '"Bereits gesehen"-Markierung';
         this.description = 'Markiert bereits gesehene Medien.';
+        this.markOwnFavoritesAsViewed = Settings.get('ViewedPostsMarker.settings.mark_own_favorites_as_viewed');
     }
 
     load() {
@@ -13,9 +14,17 @@ export default class ViewedPostsMarker {
         this.styles = require('../style/viewedPostsMarker.less');
         this.viewedPosts = ViewedPostsMarker.getViewedPosts();
 
-        $(document).ajaxComplete((event, request, settings) => {
-            for (let i = 0; i < this.viewedPosts.length; i++) {
-                ViewedPostsMarker.markAsViewed(this.viewedPosts[i]);
+        // TODO: Fire event only once
+        $(document).ajaxComplete(async (event, request, settings) => {
+            /* Since this is a global event handler for every ajax we need to specify on which event 
+             * it should be fired. This is the case for every event which accesses items.
+             */
+            if(settings.url.startsWith('/api/items/get')) {
+                if(this.markOwnFavoritesAsViewed || !(await this.wouldLoadUserCollection(settings.url))) {   
+                    this.viewedPosts.forEach(post => {
+                        ViewedPostsMarker.markAsViewed(post);
+                    });
+                }
             }
         });
 
@@ -49,6 +58,16 @@ export default class ViewedPostsMarker {
         return JSON.parse(posts);
     }
 
+    getSettings() {
+        return [
+            {
+                id: 'mark_own_favorites_as_viewed',
+                title: 'Eigene Favoriten ebenfalls als gelesen markieren',
+                description: 'Markiert Posts in den persönlichen Sammlungen als gelesen'
+            }
+        ];
+    }
+
     addViewedPost(id) {
         if (this.viewedPosts.length >= 10000) {
             this.viewedPosts = this.viewedPosts.slice(-10000);
@@ -59,5 +78,45 @@ export default class ViewedPostsMarker {
         }
 
         Settings.set('viewed_posts', JSON.stringify(this.viewedPosts));
+    }
+
+    /**
+     * Retrieves the current logged in username by accessing the API
+     * @returns {Promise<string>}
+     */
+    getLoggedInUserName() {
+        return new Promise((resolve, reject) => {
+            GM_xmlhttpRequest({
+                method: "GET",
+                url: "https://pr0gramm.com/api/user/name",
+                headers: {
+                    Accept: "application/json"
+                },
+                onerror: (/**@type {Response}*/err) => {
+                    reject(err);
+                },
+                onload: (/**@type {Response}*/res) => {
+                    let body = JSON.parse(res.response);
+                    let name = body.name;
+                    if(name) {
+                        resolve(name);
+                    }
+                    reject('Name not found')
+                }
+            })
+        });
+    }
+
+    /**
+     * Checks any url whether this url would load any of the logged in users collection
+     * @param {string} url API Request URL
+     * @returns {boolean}
+     */
+    async wouldLoadUserCollection(url) {
+        const name = await this.getLoggedInUserName();
+        const queryParams = new URLSearchParams(url);
+
+        // If the current request would retrieve any of the users collection
+        return queryParams.has("collection") && queryParams.get("user") === name;
     }
 }
