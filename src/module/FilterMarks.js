92b228deaf50cc52b5084d4ad5d5987d493e8a73
Tobi <22715034+twobiers@users.noreply.github.com>
2022-04-20 22:24:26 +0200

    fix: show thumbnails again

--- src/module/FilterMarks.js
+++ src/module/FilterMarks.js
@@ -70,7 +70,7 @@ export default class FilterMarks {
 
         // Handle stream-view
         p.View.Stream.Main.prototype.buildItem = function (item) {
-            let content = `<a class="silent thumb filter ${_this.displayLabelStream ? FilterMarks.getFilter(item) : ''}" id="item-${item.id}" href="${this.baseURL + item.id}"><img src="${item.thumb}"/> ${item.promoted > 1000000000 ? '<div class="sticky-badge"></div>' : ''}`;
+            let content = `<a class="silent thumb filter ${_this.displayLabelStream ? FilterMarks.getFilter(item) : ''}" id="item-${item.id}" href="${this.baseURL + item.id}"><img onload="this.classList.add(\'loaded\')" src="${item.thumb}"/> ${item.promoted > 1000000000 ? '<div class="sticky-badge"></div>' : ''}`;
 
             if (_this.displayBenisStream) {
                 content += `<span class="benis-info ${item.up - item.down > 0 ? 'up' : 'down'}">${item.up - item.down}</span></a>`;
