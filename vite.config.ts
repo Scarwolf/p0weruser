b3835375997202867f4a453541c3b984f57f4133
twobiers <22715034+twobiers@users.noreply.github.com>
2022-05-16 14:12:33 +0200

    chore: add customCSS

--- vite.config.ts
+++ vite.config.ts
@@ -17,7 +17,10 @@ export default defineConfig(({ mode }) => ({
                     connect: ['rep0st.rene8888.at', 'github.com', 'raw.githubusercontent.com', 'pr0gramm.com', 'pr0p0ll.com'],
                     'run-at': 'document-end',
                     grant: ['GM_notification', 'GM_xmlhttpRequest'],
-                    require: 'https://code.jquery.com/ui/1.13.1/jquery-ui.min.js',
+                    require: [ 'https://code.jquery.com/ui/1.13.1/jquery-ui.min.js' ],
+                    resource: {
+                        'customCSS': 'https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css'
+                    },
                     // The -dev suffix might not be enough to provide a continous dev version. It should work if the script is also 
                     // newer to still receive an update but this is different between script engines. If so, we most likely need to 
                     // append another suffix (Maybe Date or GitHub Run ID).
