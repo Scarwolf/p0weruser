435666aa5ee31f2b8d5cbcf267743e2396e1c671
twobiers <22715034+twobiers@users.noreply.github.com>
2022-10-01 15:30:15 +0200

    add option to hide benis (fixes #106)

--- src/module/StyleCustomization/StyleCustomization.ts
+++ src/module/StyleCustomization/StyleCustomization.ts
@@ -9,6 +9,7 @@ export default class StyleCustomization implements PoweruserModule {
     readonly name = 'Stilisierung';
     readonly description = 'Einzelne Styleanpassungen';
     isTransparentNavbarEnabled = Settings.get(`${this.id}.settings.transparent_navbar`);
+    hideBenis = Settings.get(`${this.id}.settings.hide_benis`);
 
 
     async load() {
@@ -16,16 +17,34 @@ export default class StyleCustomization implements PoweruserModule {
             document.getElementById("head")?.classList.add("transparent");
         }
         loadStyle(style);
+
+        if(this.hideBenis === true) {
+            const hideBenisStyle = document.createElement("style")
+            hideBenisStyle.innerText = `
+                .score, .user-score {
+                    color: transparent;
+                }
+            `;
+
+            document.head.appendChild(hideBenisStyle);
+        }
     }
 
     getSettings(): ModuleSetting[] {
         return [
-            {
-                id: 'transparent_navbar',
-                title: 'Transparente Navigationsleiste',
-                description: 'Die Navigationsleiste wird transparent angezeigt. Inspieriert von <a href="https://github.com/holzmaster/augenzuckerl">Augenzuckerl</a> von <a href="https://pr0gramm.com/user/holzmaster">@holzmaster</a>. Außerdem wird das Navigationsmenü auch im Widescreen Mode zentriert dargestellt.',
-                type: "checkbox"
-            }
+          {
+            id: "transparent_navbar",
+            title: "Transparente Navigationsleiste",
+            description:
+              'Die Navigationsleiste wird transparent angezeigt. Inspieriert von <a href="https://github.com/holzmaster/augenzuckerl">Augenzuckerl</a> von <a href="https://pr0gramm.com/user/holzmaster">@holzmaster</a>. Außerdem wird das Navigationsmenü auch im Widescreen Mode zentriert dargestellt.',
+            type: "checkbox",
+          },
+          {
+            id: "hide_benis",
+            title: "Versteckt den Benis",
+            description: 'Versteckt den Benis bei Posts, Kommenteren und Nutzern. Der Benis wird nur über unsichtbare Schrift versteckt und kann über eine Markierung sichtbar gemacht werden - wie Zaubertinte',
+            type: "checkbox",
+          },
         ];
     }
 }
