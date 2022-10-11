9fc489572770ed48e6c09fc7cba4c0ccb1bf85ef
twobiers <twobiers@users.noreply.github.com>
2022-10-11 17:47:13 +0000

    Deploying to gh-pages from @ Scarwolf/p0weruser@1eb189646cb2e49ecd74933c8e73a6950ca6ccdf 🚀

--- p0weruser.user.js
+++ p0weruser.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         p0weruser - Rel0aded
 // @namespace    https://github.com/Scarwolf/p0weruser/
-// @version      2.3.2
+// @version      2.4.0
 // @author       PoTTii - Created by Florian Maak
 // @description  A modularized userScript for pr0gramm.com - Developed by FlorianMaak, Modified by Scarwolf
 // @license      GPL-3.0; http://www.gnu.org/licenses/gpl-3.0.txt
